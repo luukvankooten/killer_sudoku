3c2ca1c9f55837b5bb1df88cb32a80f4e25267ea
luukvankooten <luukvankooten@gmail.com>
2022-05-13 15:47:13 +0200

    Proper calculate the sum of the group

--- src/index.js
+++ src/index.js
@@ -15,11 +15,13 @@ class Item {
 	toString() {
 		let number = this.value.toString();
 
-		if(this.value === 0) {
+		// console.log(number);
+
+		if (this.value === 0) {
 			number = this.totalSum.toString();
 		}
 
-		if(number.length === 1) {
+		if (number.length === 1) {
 			number = number.concat(' ');
 		}
 
@@ -30,22 +32,22 @@ class Item {
 class TotalSum {
 
 	/**
-	 * @param  {Number} totalSum
+	 * @param  {Number} totalSum [description]
+	 * @param  {Group} group    [description]
 	 * @return {[type]}          [description]
 	 */
-	constructor(totalSum) {
+	constructor(totalSum, group) {
 		this.orginalSum = totalSum;
 		this.totalSum = totalSum;
+		this.group = group;
 	}
 
 	canSubtract(num) {
-		const sub = this.orginalSum - num;
+		const sumOf = this.group.items.reduce((p, c) => p + c.value, 0);
 
-		if(sub <= 0) {
-			return false;
-		}
+		// console.log(this.totalSum, sumOf, num);
 
-		return true;
+		return this.totalSum - (sumOf + num) >= 0;
 	}
 
 	toString() {
@@ -59,16 +61,10 @@ class Group {
 	 * @param  {number[]} positions [description]
 	 */
 	constructor(totalSum, positions) {
-		this.totalSum = new TotalSum(totalSum);
+		const sum = new TotalSum(totalSum, this);
+		this.totalSum = sum;
 		this.positions = positions;
-		this._items = positions.map(p => new Item(p, this.totalSum));
-	}
-
-	/**
-	 * @return {Item[]} [description]
-	 */
-	get items() {
-		return this._items;
+		this.items = positions.map(p => new Item(p, sum));
 	}
 }
 
@@ -92,11 +88,11 @@ class Board {
 			.map(g => g.items)
 			.flat()
 			.sort((a, b) => {
-				if(a.position > b.position) {
+				if (a.position > b.position) {
 					return 1;
 				}
 
-				if(a.position < b.position) {
+				if (a.position < b.position) {
 					return -1;
 				}
 
@@ -111,23 +107,21 @@ class Board {
 	solve(board) {
 		const empty = this.findEmpty(board);
 
-		if(!empty) {
+		if (!empty) {
 			return true;
 		}
 
 		const [row, col] = empty;
 
 		for (const i of _.range(1, 10)) {
-			if(this.valid(board, i, empty)) {
+			if (this.valid(board, i, row, col)) {
 				const item = board[row][col];
 				item.value = i;
-				item.totalSum.totalSum = item.totalSum.totalSum - i;
 
-				if(this.solve(board)) {
+				if (this.solve(board)) {
 					return true;
 				}
 
-				item.totalSum.totalSum = item.totalSum.orginalSum;
 				item.value = 0;
 			} 
 		}
@@ -141,19 +135,17 @@ class Board {
 	 * @param  {[Number, Number]} pos
 	 * @return {Boolean}      [description]
 	 */
-	valid(board, num, pos) {
-		const [row, col] = pos;
-
+	valid(board, num, row, col) {
 		//Check row
-		for(const i of _.range(board[0].length)) {
-			if(board[row][i].value === num && col !== i) {
+		for (const i of _.range(board[0].length)) {
+			if (board[row][i].value === num && col !== i) {
 				return false;
 			}
 		}
 
 		//Check column
-		for(const i of _.range(board.length -1)) {
-			if(board[i][col].value === num && row !== i) {
+		for (const i of _.range(board.length - 1)) {
+			if (board[i][col].value === num && row !== i) {
 				return false;
 			}
 		}
@@ -162,18 +154,19 @@ class Board {
 		let box_x = Math.floor(col / 3);
 		let box_y = Math.floor(row / 3);
 
-		for (const i of _.range(box_y*3, box_y*3 + 3)) {
-			for(const j of _.range(box_x * 3, box_x*3 + 3)) {
-				if(board[i][j]?.value === num && row !== i && col !== j) {
+		for (const i of _.range(box_y * 3, box_y * 3 + 3)) {
+			for (const j of _.range(box_x * 3, box_x * 3 + 3)) {
+				if (board[i][j]?.value === num && row !== i && col !== j) {
 					return false;
 				}
 			}
 		}
-		
-		//Check totalSum
+
 		let item = board[row][col];
 
-		if(item.totalSum <= 0) {
+		const subtract = item.totalSum.canSubtract(num);
+
+		if (!subtract) {
 			return false;
 		}
 
@@ -186,10 +179,9 @@ class Board {
 	 * @return {[Number, Number]} first row second column
 	 */
 	findEmpty(board) {
-		for(const [row, items] of board.entries()) {
-			for(const [col, item] of items.entries()) {
-				if(item.value === 0)
-				{
+		for (const [row, items] of board.entries()) {
+			for (const [col, item] of items.entries()) {
+				if (item.value === 0) {
 					return [row, col];
 				}
 			}
@@ -204,36 +196,36 @@ class Board {
 
 	draw() {
 		this.items
-		.reduce((resultArray, item, index) => { 
-		  const chunkIndex = Math.floor(index/9);
+			.reduce((resultArray, item, index) => { 
+				const chunkIndex = Math.floor(index / 9);
 
-		  if(!resultArray[chunkIndex]) {
-		    resultArray[chunkIndex] = [] // start a new chunk
-		  }
-
-		  resultArray[chunkIndex].push(item)
-
-		  return resultArray
-		}, []).forEach((row, i) => {
-			let str = "";
-
-			row.forEach((item, index) => {
-				str += ` ${item} `;
-
-				if(index % 3 === 2 && index % 9 !== 8) {
-					str += " | ";
+				if (!resultArray[chunkIndex]) {
+					resultArray[chunkIndex] = [] // start a new chunk
 				}
 
-				if(index % 9 === 8) {
-					console.log(str);	
-					str = "";
-				}
-			});
+				resultArray[chunkIndex].push(item)
 
-			if(i % 3 === 2 && i % 8 !== 0 ) {
-				console.log('-----------------------------------------');
-			}
-		})
+				return resultArray
+			}, []).forEach((row, i) => {
+				let str = "";
+
+				row.forEach((item, index) => {
+					str += ` ${item} `;
+
+					if (index % 3 === 2 && index % 9 !== 8) {
+						str += " | ";
+					}
+
+					if (index % 9 === 8) {
+						console.log(str);	
+						str = "";
+					}
+				});
+
+				if (i % 3 === 2 && i % 8 !== 0) {
+					console.log('-----------------------------------------');
+				}
+			})
 	}
 }
 
@@ -245,7 +237,7 @@ const board = new Board([
 	new Group(22, [5, 13, 14, 22]),
 	new Group(4, [6, 15]),
 	new Group(16, [7, 16]),
-	new Group(15, [8,17,26,35]),
+	new Group(15, [8, 17, 26, 35]),
 
 	//Row 2
 	new Group(25, [9, 10, 18, 19]),
@@ -253,28 +245,28 @@ const board = new Board([
 
 	//Row 3
 	new Group(9, [20, 21, 30]),
-	new Group(8, [23,32,41]),
-	new Group(20, [24,25,33]),
+	new Group(8, [23, 32, 41]),
+	new Group(20, [24, 25, 33]),
 
 	//Row 4
-	new Group(6, [27,36]),
-	new Group(14, [28,29]),
-	new Group(17, [31,40,49]),
-	new Group(17, [34,43,42]),
+	new Group(6, [27, 36]),
+	new Group(14, [28, 29]),
+	new Group(17, [31, 40, 49]),
+	new Group(17, [34, 43, 42]),
 
 	//Row 5
-	new Group(13, [37,38,46]),
-	new Group(20, [39,48,57]),
-	new Group(12, [44,53]),
+	new Group(13, [37, 38, 46]),
+	new Group(20, [39, 48, 57]),
+	new Group(12, [44, 53]),
 
 	//Row 6
-	new Group(27, [45,54,63,72]),
-	new Group(6, [47,55,56]),
-	new Group(20, [50,59,60]),
+	new Group(27, [45, 54, 63, 72]),
+	new Group(6, [47, 55, 56]),
+	new Group(20, [50, 59, 60]),
 	new Group(6, [51, 52]),
 
 	//Row 7
-	new Group(10, [58, 67,66,75]),
+	new Group(10, [58, 67, 66, 75]),
 	new Group(14, [61, 62, 70, 71]),
 
 	//Row 8
